654aa252b6c8220e6b9775125c89a41062672a56
Team7034 <33676445+Team7034@users.noreply.github.com>
2023-08-01 19:39:04 -0700

    ae

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,8 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+import java.io.IOException;
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -27,7 +29,11 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+    try {
+      m_robotContainer = new RobotContainer();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   /**
---
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -0,0 +1,73 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.io.File;
+import java.io.IOException;
+
+import edu.wpi.first.wpilibj.Filesystem;
+import swervelib.SwerveController;
+import swervelib.parser.SwerveParser;
+import swervelib.SwerveDrive;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+
+
+public class SwerveSubsystem extends SubsystemBase {
+    File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve/neo");
+    SwerveDrive swerveDrive  = new SwerveParser(swerveJsonDirectory).createSwerveDrive();
+
+    // With eager singleton initialization, any static variables/fields used in the 
+    // constructor must appear before the "INSTANCE" variable so that they are initialized 
+    // before the constructor is called when the "INSTANCE" variable initializes.
+
+    /**
+     * The Singleton instance of this SwerveSubsystem. Code should use
+     * the {@link #getInstance()} method to get the single instance (rather
+     * than trying to construct an instance of this class.)
+     */
+    private final static SwerveSubsystem INSTANCE;
+
+    static {
+        try {
+            INSTANCE = new SwerveSubsystem();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Returns the Singleton instance of this SwerveSubsystem. This static method
+     * should be used, rather than the constructor, to get the single instance
+     * of this class. For example: {@code SwerveSubsystem.getInstance();}
+     */
+    @SuppressWarnings("WeakerAccess")
+    public static SwerveSubsystem getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * Creates a new instance of this SwerveSubsystem. This constructor
+     * is private since this class is a Singleton. Code should use
+     * the {@link #getInstance()} method to get the singleton instance.
+     */
+    public SwerveSubsystem() throws IOException {
+    }
+
+    public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop)
+    {
+        swerveDrive.drive(translation, rotation, fieldRelative, isOpenLoop);
+    }
+
+    public SwerveController getSwerveController() {
+        return swerveDrive.getSwerveController();
+    }
+
+    public Rotation2d getHeading() {
+        return swerveDrive.getYaw();
+    }
+}
+
