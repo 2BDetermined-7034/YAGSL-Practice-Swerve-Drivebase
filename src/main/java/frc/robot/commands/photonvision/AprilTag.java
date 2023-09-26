232d74e10f1d7a0364246853faa9d700f5550153
npspy <username.lilrice@gmail.com>
2023-09-25 18:27:15 -0700

    ??

---
+++ src/main/java/frc/robot/commands/photonvision/AprilTag.java
@@ -0,0 +1,29 @@
+package frc.robot.commands.photonvision;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import org.photonvision.PhotonCamera;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class AprilTag extends Command {
+    // Change this to match the name of your camera
+    private final PhotonCamera camera = new PhotonCamera("photonvision");
+
+    public AprilTag() {
+
+    }
+    @Override
+    public void execute() {
+        // Get the latest result from PhotonVision
+        var result = camera.getLatestResult();
+
+        if (result.hasTargets()) {
+            // If we have targets, get the pose of the best target
+            var pose = result.getBestTarget().getBestCameraToTarget();
+
+            // Convert the pose's translation to a Translation2d object
+            var translation = new Translation2d(pose.getTranslation().getX(), pose.getTranslation().getY());
+
+            System.out.println("Best Target Translation: " + translation);
+        }
+    }
+}
---
+++ src/main/java/frc/robot/commands/photonvision/DriveToAprilTag.java
@@ -0,0 +1,49 @@
+package frc.robot.commands.photonvision;
+
+import org.photonvision.PhotonCamera;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class DriveToAprilTag extends Command {
+    // Change this to match the name of the camera
+    private final PhotonCamera camera = new PhotonCamera("photonvision");
+
+    SwerveSubsystem swerveDrive;
+
+    public DriveToAprilTag(SwerveSubsystem swerveSubsystem) {
+        this.swerveDrive = swerveSubsystem;
+    }
+
+    @Override
+    public void execute() {
+        // Get the latest result from PhotonVision
+        var result = camera.getLatestResult();
+
+        if (result.hasTargets()) {
+            // Get the pose of the best target
+            var pose = result.getBestTarget().getBestCameraToTarget();
+
+            // Convert the pose's translation to a Translation2d object
+            var translation = new Translation2d(pose.getTranslation().getX(), pose.getTranslation().getY());
+
+            // Calculate the current distance to the target
+            double currentDistance = translation.getNorm();
+
+            // Calculate the error in distance
+            // Desired distance to maintain from the AprilTag (in meters)
+            double desiredDistance = 2.0;
+            double distanceError = desiredDistance - currentDistance;
+
+            // Use a simple proportional controller to drive towards or away from the target
+            double speed = 0.1 * distanceError;
+
+            // Get the yaw of the best target
+            double yaw = result.getBestTarget().getYaw();
+
+            // Use the translation and yaw to drive and rotate the robot
+            swerveDrive.drive(translation.rotateBy(new Rotation2d(speed)), yaw, false, false);
+        }
+    }
+}
